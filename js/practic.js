401bba6004793c4e0928c0c88286b6f66ae6c9de
Sherzod <96307270+sherzod66@users.noreply.github.com>
2023-08-14 18:59:34 +0500

    Delete js directory

--- js/practic.js
+++
@@ -1,8 +0,0 @@
-let arr = [
-	{ name: 'Вася', age: 18 },
-	{ name: 'Шерзод', age: 18 },
-	{ name: 'Амир ', age: 17 },
-];
-
-let resultOne = arr.findIndex(item => item.age === 17);
-console.log(resultOne)
